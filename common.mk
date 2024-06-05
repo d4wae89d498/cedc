c9dba0fcda8e55092e1a0f41c31daeec7aadd888
ddwadwa <faussurier.marc@icloud.com>
2024-06-05 09:29:37 +0200

    makefile

--- common.mk
+++ common.mk
@@ -19,19 +19,19 @@ DEP_DIR := $(TMP_DIR)/dep
 # C++ settings
 CXX := clang++ -std=c++26
 CXXFLAGS := -g \
-            -O0 \
-            -fsanitize=address \
-            -nostdinc++ \
-            -nostdlib++ \
-            -I$(PROJECT_DIR)/third-party/llvm-project/build/include/c++/v1 \
-            -Wno-unqualified-std-cast-call \
-            -fprebuilt-module-path=$(PROJECT_DIR)/$(TMP_DIR)/pcm \
-            -I$(PROJECT_DIR)/third-party/antlr4/runtime/Cpp/runtime/src \
-            -I$(PROJECT_DIR)/third-party/libastmatcher-parser \
-            -Wno-unused-command-line-argument \
-            -Wreserved-module-identifier
+			-O0 \
+			-fsanitize=address \
+			-nostdinc++ \
+			-nostdlib++ \
+			-I$(PROJECT_DIR)/third-party/llvm-project/build/include/c++/v1 \
+			-Wno-unqualified-std-cast-call \
+			-fprebuilt-module-path=$(PROJECT_DIR)/$(TMP_DIR)/pcm \
+			-I$(PROJECT_DIR)/third-party/antlr4/runtime/Cpp/runtime/src \
+			-I$(PROJECT_DIR)/third-party/libastmatcher-parser \
+			-Wno-unused-command-line-argument \
+			-Wreserved-module-identifier
 
 # Avoid llvm-libcxx iostream ABI error on macos (https://github.com/frida/frida-gum/issues/694)
 ifeq ($(shell uname -s), Darwin)
-    CXXFLAGS += -target x86_64-apple-macos10.9
+	CXXFLAGS += -target x86_64-apple-macos10.9
 endif
