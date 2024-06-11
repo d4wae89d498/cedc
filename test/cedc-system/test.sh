cc493631ef90e487a670349a7d77a2e20d6e25c5
ddwadwa <faussurier.marc@icloud.com>
2024-06-11 12:11:14 +0200

    system test

--- test/cedc-system/test.sh
+++ test/cedc-system/test.sh
@@ -6,16 +6,16 @@ echo "cc location: $(type cc)"
 echo "c++ location: $(type c++)"
 echo "LDFLAGS: $LDFLAGS"
 echo "CXXFLAGS: $CXXFLAGS"
-$CC /test.cpp -o /test_cc && echo "CC compilation successful"
-$CXX /test.cpp -o /test_cxx && echo "CXX compilation successful"
-clang ./test.cpp -o /test_clang && echo "Clang compilation successful"
-clang++ ./test.cpp -o /test_clangpp && echo "Clang++ compilation successful"
-cc ./test.cpp -o /test_cc_alias && echo "cc alias compilation successful"
-c++ ./test.cpp -o /test_cxx_alias && echo "c++ alias compilation successful"
+$CC /test.cpp -o ./test_CC && echo "'CC' compilation successful"
+$CXX /test.cpp -o ./test_CXX && echo "'CXX' compilation successful"
+clang ./test.cpp -o ./test_clang && echo "'clang' compilation successful"
+clang++ ./test.cpp -o ./test_clangpp && echo "'Clang++' compilation successful"
+cc ./test.cpp -o ./test_cc_alias && echo "'cc' alias compilation successful"
+c++ ./test.cpp -o ./test_cxx_alias && echo "'c++' alias compilation successful"
 echo "Running 'CC' compiled binary:"
-./test_cc
+./test_CC
 echo "Running 'CXX' compiled binary:"
-./test_cxx
+./test_CXX
 echo "Running 'clang' compiled binary:"
 ./test_clang
 echo "Running 'clang++' compiled binary:"
